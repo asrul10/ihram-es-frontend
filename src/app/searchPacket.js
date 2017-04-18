9b5af74bc161ede3dd03263979613bc3d54d3d19
Asrul Hanafi <hanafi.asrul@gmail.com>
2017-04-18 14:16:01 +0700

    Change filter in header

--- src/app/searchPacket.js
+++ src/app/searchPacket.js
@@ -2,33 +2,9 @@ function searchController($scope, $http) {
 	// $('select.dropdown').dropdown();
 	var ctrl = this;
 
-	ctrl.model = {
-		selectedGroup: null
-	};
-
-	// Get list program
-	ctrl.getProgram = function() {
-		var idGroup = ctrl.model.selectedGroup;
-		$http({
-			method: 'GET',
-			url: services + '/front/getProgram?idGroup=' + idGroup
-		}).then(getData, errorData);
-	};
-
-	function getData(response) {
-		var optionsProgram = [];
-        optionsProgram.push('<option value="">Pilih Program</option>');
-		if (response.data) {
-			angular.forEach(response.data, function(value, key) {
-	            optionsProgram.push('<option value="' + value.id + '">' + value.name + '</option>');
-			});
-		}
-		$('#ihram-select-program').html(optionsProgram.join(''));
-	}
-
-	function errorData(response) {
-		console.log('Error 404');
-	}
+	$('#search-tanggal').calendar({
+		type: 'month'
+	});	
 }
 
 angular
